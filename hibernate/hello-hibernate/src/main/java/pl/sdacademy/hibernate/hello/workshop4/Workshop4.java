5383a7f631b4dcb73ec04e5b240f0ced0552034e
Piotr Kachnowicz <p.kachnowicz@wp.pl>
2021-05-19 17:40:06 +0200

    hibernate workshop5

--- hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop4/Workshop4.java
+++ hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop4/Workshop4.java
@@ -1,7 +1,12 @@
 package pl.sdacademy.hibernate.hello.workshop4;
 
 import pl.sdacademy.hibernate.hello.common.City;
+import pl.sdacademy.hibernate.hello.common.Country;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -17,6 +22,18 @@ public class Workshop4 {
     }
 
     public static List<City> getCities(String countryCode) {
-        throw new UnsupportedOperationException("TODO");
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("HelloHibernatePU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            TypedQuery<City> typedQuery = entityManager.createQuery(
+                    "SELECT c FROM City c JOIN FETCH c.country WHERE c.country.code = :countryCode " +
+                            "ORDER BY c.name", City.class
+            );
+            typedQuery.setParameter("countryCode", countryCode);
+            return typedQuery.getResultList();
+        } finally {
+            entityManagerFactory.close();
+        }
     }
 }
--- hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop5/Workshop5.java
+++ hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop5/Workshop5.java
@@ -1,5 +1,6 @@
 package pl.sdacademy.hibernate.hello.workshop5;
 
+import javax.persistence.*;
 import java.util.*;
 
 public class Workshop5 {
@@ -7,7 +8,7 @@ public class Workshop5 {
         System.out.println("Podaj kody krajów rozdzielone przecinkami:");
         final String countryCodes = new Scanner(System.in).nextLine();
 
-        Map<String,List<String>> map = getCities(countryCodes.split(","));
+        Map<String, List<String>> map = getCities(countryCodes.split(","));
 
         final StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, List<String>> entry : map.entrySet()) {
@@ -20,7 +21,26 @@ public class Workshop5 {
         System.out.println(sb);
     }
 
-    public static Map<String,List<String>> getCities(String... codes) {
-        throw new UnsupportedOperationException("TODO");
+    public static Map<String, List<String>> getCities(String... codes) {
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory("HelloHibernatePU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String query = "SELECT c.name AS cityName, c.country.name AS countryName FROM City c " +
+                "JOIN c.country WHERE c.country.code IN :countryCodes ORDER BY c.name";
+        try {
+            TypedQuery<Tuple> typedQuery = entityManager.createQuery(
+                    query, Tuple.class
+            );
+            List<Tuple> tupleList = typedQuery.setParameter("countryCodes", Set.of(codes)).getResultList();
+            Map<String, List<String>> tupleMap = new LinkedHashMap<>();
+            for (Tuple tuple : tupleList) {
+                String cityName = tuple.get("cityName", String.class);
+                String countryName = tuple.get("countryName", String.class);
+                tupleMap.computeIfAbsent(countryName, x -> new LinkedList<>()).add(cityName);
+            }
+            return tupleMap;
+        } finally {
+            entityManagerFactory.close();
+        }
     }
 }
